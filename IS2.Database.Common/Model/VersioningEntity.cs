e041e6f113030de49d471ff3a809a2d379ba3de5
rapaner <rapaner@yandex.ru>
2023-05-20 15:04:39 +0300

    Добавлен общий класс для сущностей с версионированием VersioningEntity Добавлены классы Setting и UserSetting

--- IS2.Database.Common/Model/VersioningEntity.cs
+++ IS2.Database.Common/Model/VersioningEntity.cs
@@ -5,19 +5,80 @@
     /// </summary>
     public abstract class VersioningEntity
     {
+        #region Поля
+
+        protected int? _requestedHashCode;
+
+        #endregion Поля
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="id">Идентификатор строки</param>
+        /// <param name="versionId">Идентификатор версии</param>
+        /// <param name="dateInsert">Дата вставки записи</param>
+        /// <param name="isDeleted">Удалена?</param>
+        protected VersioningEntity(Guid id, Guid versionId, DateTime dateInsert, bool isDeleted)
+        {
+            Id = id;
+            VersionId = versionId;
+            DateInsert = dateInsert;
+            IsDeleted = isDeleted;
+        }
+
+        #region Свойства
+
+        /// <summary>
+        /// Идентификатор строки
+        /// </summary>
+        public Guid Id { get; protected set; }
+
         /// <summary>
         /// Идентификатор версии
         /// </summary>
-        public Guid VersionId { get; set; }
+        public Guid VersionId { get; protected set; }
 
         /// <summary>
         /// Дата вставки записи
         /// </summary>
-        public DateTime DateInsert { get; set; }
+        public DateTime DateInsert { get; protected set; }
 
         /// <summary>
         /// Удалена?
         /// </summary>
-        public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; protected set; }
+
+        #endregion Свойства
+
+        #region Методы изменения свойств
+
+        /// <summary>
+        /// Установить код версии
+        /// </summary>
+        /// <param name="versionId">Код версии</param>
+        public void SetVersionId(Guid versionId)
+        {
+            VersionId = versionId;
+        }
+
+        /// <summary>
+        /// Установить дату вставки
+        /// </summary>
+        /// <param name="dateInsert">Дата вставки</param>
+        public void SetDateInsert(DateTime dateInsert)
+        {
+            DateInsert = dateInsert;
+        }
+
+        /// <summary>
+        /// Установить признак удаления
+        /// </summary>
+        /// <param name="isDeleted">Удалена?</param>
+        public void SetIsDeleted(bool isDeleted)
+        {
+            IsDeleted = isDeleted;
+        }
+
+        #endregion Методы изменения свойств
     }
 }
---
+++ IS2.Database.ConfigurationData/Model/Setting.cs
@@ -0,0 +1,117 @@
+﻿using IS2.Database.Common.Model;
+
+namespace IS2.Database.ConfigurationData.Model
+{
+    /// <summary>
+    /// Настройки
+    /// </summary>
+    public class Setting : VersioningEntity
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="id">Идентификатор сущности</param>
+        /// <param name="settingId">Идентификатор настройки</param>
+        /// <param name="name">Название</param>
+        /// <param name="versionId">Идентификатор версии</param>
+        /// <param name="dateInsert">Дата вставки записи</param>
+        /// <param name="isDeleted">Удалена?</param>
+        public Setting(Guid id, Guid settingId, string name, Guid versionId, DateTime dateInsert, bool isDeleted) : base(id, versionId, dateInsert, isDeleted)
+        {
+            SettingId = settingId;
+            Name = name;
+        }
+
+        #region Свойства
+
+        /// <summary>
+        /// Идентификатор настройки
+        /// </summary>
+        public Guid SettingId { get; protected set; }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; protected set; }
+
+        #endregion Свойства
+
+        #region Методы изменения свойств
+
+        /// <summary>
+        /// Изменить идентификатор настройки
+        /// </summary>
+        /// <param name="settingId">Идентификатор настройки</param>
+        public void SetSettingId(Guid settingId)
+        {
+            SettingId = settingId;
+        }
+
+        /// <summary>
+        /// Изменить название
+        /// </summary>
+        /// <param name="name">Название</param>
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        #endregion Методы изменения свойств
+
+
+        #region Служебные методы
+
+        public bool IsTransient()
+        {
+            return Id == default;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Setting))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            Setting item = (Setting)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+            else
+                return item.SettingId == SettingId
+                    && item.Name == Name;
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+
+        public static bool operator ==(Setting left, Setting right)
+        {
+            if (Equals(left, null))
+                return (Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        public static bool operator !=(Setting left, Setting right)
+        {
+            return !(left == right);
+        }
+
+        #endregion Служебные методы
+    }
+}
