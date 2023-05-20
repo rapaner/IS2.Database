8c90d2c57bdfaa190164ac2d7a0f2c25a957828c
rapaner <rapaner@yandex.ru>
2023-05-20 14:31:34 +0300

    Add project files.

---
+++ IS2.Database.Common/Model/VersioningEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace IS2.Database.Common.Model
+{
+    /// <summary>
+    /// Сущность, поддерживающая версионность
+    /// </summary>
+    public abstract class VersioningEntity
+    {
+        /// <summary>
+        /// Идентификатор версии
+        /// </summary>
+        public Guid VersionId { get; set; }
+
+        /// <summary>
+        /// Дата вставки записи
+        /// </summary>
+        public DateTime DateInsert { get; set; }
+
+        /// <summary>
+        /// Удалена?
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
