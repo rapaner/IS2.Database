29f08ad91b1d3188f7834c6fb448259d47931041
rapaner <rapaner@yandex.ru>
2023-05-23 21:32:36 +0300

    Добавлены заготовки для всех проектов

---
+++ IS2.Database.ManagementData/EntityConfiguration/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS2.Database.ConfigurationData.EntityConfiguration
+{
+    internal class Configuration
+    {
+    }
+}
